08b6780839a6a3662a141a1b7348894a4e1eb365
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-05-12 13:42:10 -0300

    Cartão de Serviço e Formulário de Serviço

---
+++ src/components/service/ServiceForm.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import Input from '../form/Input'
+import SubmitButton from '../form/SubmitButton'
+import styles from '../project/ProjectForm.module.css'
+
+function ServiceForm({handleSubmit, textBtn, projectData}){
+
+    const [service, setService] = useState({})
+
+    function submit(e){
+        e.preventDefault()
+        projectData.services.push(service)
+        handleSubmit(projectData)
+    }
+
+    function handleChange(e){
+        setService({...service, [e.target.name]: e.target.value})
+    }
+
+    return (
+
+        <form onSubmit={submit} className={styles.form}>
+
+            <Input
+            type="text"
+            text="Nome do Serviço"
+            name="name"
+            placeholder="Insira o nome do serviço"
+            handleOnChange={handleChange}
+            />
+
+            <Input
+            type="number"
+            text="Custo do Serviço"
+            name="cost"
+            placeholder="Insira o valor total"
+            handleOnChange={handleChange}
+            />
+
+            <Input
+            type="text"
+            text="Descrição do Serviço"
+            name="description"
+            placeholder="Descreva o serviço"
+            handleOnChange={handleChange}
+            />
+
+            <SubmitButton text={textBtn}/>
+
+        </form>
+
+    )
+
+}
+
+export default ServiceForm
